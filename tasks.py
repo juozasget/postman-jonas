1249fe33a757c4a81541629775dbb7945cae3b7d
juozasget <juozasget@gmail.com>
2018-06-18 13:58:57 +0300

    update

--- tasks.py
+++ tasks.py
@@ -1,5 +1,10 @@
+import os
+import sys
+import json
 from celery import Celery
 from celery.schedules import crontab
+
+from app import set_broadcast, send_broadcast
 
 BROKER_URL = os.environ['REDIS_URL']
 
@@ -10,12 +15,15 @@ app = Celery()
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, send.s(), expires=10)
     # Executes every Monday morning at 7:30 a.m.
     sender.add_periodic_task(
         crontab(hour=7, minute=30, day_of_week=1),
-        test.s('Happy Mondays!'),
+        send.s(),
     )
 
 @app.task
-def test(arg):
-    print(arg)
+def send():
+    message_creative_id = set_broadcast()   #Send the message to fb
+    send_broadcast(message_creative_id)
